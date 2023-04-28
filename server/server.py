9bac8b0a474fe460564fd337d54aa5f98ab7854c
Omar Trejo <trejo.omar562@csu.fullerton.edu>
2023-04-27 19:41:08 -0700

    Mostly done couldn't get it running

--- server/server.py
+++ server/server.py
@@ -1,40 +1,123 @@
+import socket
+import sys
+import time
+import os
+import struct
 
-# first of all import the socket library
-import socket            
- 
-# next create a socket object
-s = socket.socket()        
-print ("Socket successfully created")
- 
-# reserve a port on your computer in our
-# case it is 12345 but it can be anything
-port = 12345               
- 
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests
-# coming from other computers on the network
-s.bind(('', port))        
-print ("socket binded to %s" %(port))
- 
-# put the socket into listening mode
-s.listen(5)    
-print ("socket is listening")           
- 
-# a forever loop until we interrupt it or
-# an error occurs
+print ("\nWelcome to the FTP server.\n\nTo get started, connect a client.")
+
+# Initialise socket stuff
+TCP_IP = "127.0.0.1" # Only a local server
+TCP_PORT = 12345 # Just a random choice
+BUFFER_SIZE = 1024 # Standard size
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind((TCP_IP, TCP_PORT))
+serverSocket.listen(1)
+conn, addr = serverSocket.accept()
+
+print ("\nConnected to by address: {}").format(addr)
+
+def put():
+# Send message once server is ready to recieve file details
+  conn.send("1")
+# Recieve file name length, then file name
+file_name_size = struct.unpack("h", conn.recv(2))[0]
+file_name = conn.recv(file_name_size)
+# Send message to let client know server is ready for document content
+conn.send("1")
+# Recieve file size
+file_size = struct.unpack("i", conn.recv(4))[0]
+# Initialise and enter loop to recive file content
+start_time = time.time()
+output_file = open(file_name, "wb")
+# This keeps track of how many bytes we have recieved, so we know when to stop the loop
+bytes_recieved = 0
+print ("\nRecieving...")
+while bytes_recieved < file_size:
+  l = conn.recv(BUFFER_SIZE)
+  output_file.write(l)
+  bytes_recieved += BUFFER_SIZE
+  output_file.close()
+  print ("\nRecieved file: {}").format(file_name)
+# Send upload performance details
+  conn.send(struct.pack("f", time.time() - start_time))
+  conn.send(struct.pack("i", file_size))
+
+def ls():
+  print ("Listing files...")
+# Get list of files in directory
+  listing = os.listdir(os.getcwd())
+# Send over the number of files, so the client knows what to expect (and avoid some errors)
+  conn.send(struct.pack("i", len(listing)))
+  total_directory_size = 0
+# Send over the file names and sizes whilst totaling the directory size
+  for i in listing:
+# File name size
+    conn.send(struct.pack("i", sys.getsizeof(i)))
+# File name
+    conn.send(i)
+# File content size
+    conn.send(struct.pack("i", os.path.getsize(i)))
+    total_directory_size += os.path.getsize(i)
+# Make sure that the client and server are syncronised
+    conn.recv(BUFFER_SIZE)
+# Sum of file sizes in directory
+    conn.send(struct.pack("i", total_directory_size))
+#Final check
+    conn.recv(BUFFER_SIZE)
+    print ("Successfully sent file listing")
+  return
+
+def get():
+  conn.send("1")
+  file_name_length = struct.unpack("h", conn.recv(2))[0]
+  print (file_name_length)
+  file_name = conn.recv(file_name_length)
+  print (file_name)
+if os.path.isfile(file_name):
+# Then the file exists, and send file size
+  conn.send(struct.pack("i", os.path.getsize(file_name)))
+else:
+# Then the file doesn't exist, and send error code
+  print ("File name not valid")
+  conn.send(struct.pack("i", -1))
+# Wait for ok to send file
+conn.recv(BUFFER_SIZE)
+# Enter loop to send file
+start_time = time.time()
+print ("Sending file...")
+content = open(file_name, "rb")
+# Again, break into chunks defined by BUFFER_SIZE
+l = content.read(BUFFER_SIZE)
+while l:
+  conn.send(l)
+  l = content.read(BUFFER_SIZE)
+  content.close()
+# Get client go-ahead, then send download details
+  conn.recv(BUFFER_SIZE)
+  conn.send(struct.pack("f", time.time() - start_time))
+
+def quit():
+# Send quit conformation
+    conn.send("1")
+# Close and restart the server
+conn.close()
+serverSocket.close()
+os.execl(sys.executable, sys.executable, *sys.argv)
+
 while True:
- 
-# Establish connection with client.
-  c, addr = s.accept()    
-  print ('Got connection from', addr )
- 
-  # send a thank you message to the client. encoding to send byte type.
-  c.send('Thank you for connecting'.encode())
- 
-  # Close the connection with the client
-  c.close()
-   
-  # Breaking once connection closed
-  break
+# Enter into a while loop to recieve commands from client
+  print ("\n\nWaiting for instruction")
+  data = conn.recv(BUFFER_SIZE)
+  print ("\nRecieved instruction: {}").format(data)
+# Check the command and respond correctly
+  if data == "PUT":
+    put()
+  elif data == "LS":
+    ls()
+  elif data == "GET":
+    get()
+  elif data == "QUIT":
+    quit()
+# Reset the data to loop
+data = None
